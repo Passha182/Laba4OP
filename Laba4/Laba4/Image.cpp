554610a9a5269a9cc76e4cb4ea263c134e72008a
pasha <pacha120431@gmail.com>
2019-05-28 20:19:55 +0300

    read bmp

--- Laba4/Laba4/Image.cpp
+++ Laba4/Laba4/Image.cpp
@@ -11,5 +11,26 @@ void picture::readPicture(const char *FileName){
         exit(1);
     }
     
+    fread(&header.id, sizeof(header.id), 1, image);
+    fread(&header.filesize, sizeof(header.filesize), 1, image);
+    fread(&header.reserved, sizeof(header.reserved), 1, image);
+    fread(&header.headersize, sizeof(header.headersize), 1, image);
+    fread(&header.infoSize, sizeof(header.infoSize), 1, image);
+    fread(&header.width, sizeof(header.width), 1, image);
+    fread(&header.depth, sizeof(header.depth), 1, image);
+    fread(&header.biPlanes, sizeof(header.biPlanes), 1, image);
+    fread(&header.bits, sizeof(header.bits), 1, image);
+    fread(&header.biCompression, sizeof(header.biCompression), 1, image);
+    fread(&header.biSizeImage, sizeof(header.biSizeImage), 1, image);
+    fread(&header.biXPelsPerMeter, sizeof(header.biXPelsPerMeter), 1, image);
+    fread(&header.biYPelsPerMeter, sizeof(header.biYPelsPerMeter), 1, image);
+    fread(&header.biClrUsed, sizeof(header.biClrUsed), 1, image);
+    fread(&header.biClrImportant, sizeof(header.biClrImportant), 1, image);
+    
+    int check = (header.width * 3) % 4;
+    int stride = 0;
+    if (check) {
+        stride = 4 - check;
+    }
 }
 
